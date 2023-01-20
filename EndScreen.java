2e1f70097a675dc226cae9c558be030bd8c41743
UwUltimatenub <114080866+UwUltimatenub@users.noreply.github.com>
2023-01-20 09:52:35 -0500

    Update EndScreen.java

--- EndScreen.java
+++ EndScreen.java
@@ -1,20 +1,15 @@
-import javax.swing.*;
-import javax.swing.event.MouseInputListener;
-
-import java.awt.event.ActionEvent;
-
-import java.awt.*;
-import java.awt.event.MouseEvent;
-
-
 public class EndScreen extends JPanel implements MouseInputListener{
     JLabel button = new JLabel("New Game");
+    //EndScreen class is extending JPanel and implementing MouseInputListener.
+    //JLabel button is created with text "New Game"
     public EndScreen(String status, String winner){
         super();
         addMouseListener(this);
+        //adding mouselistener to this class
         this.setBackground(new Color(0,0,0,200));
         this.setOpaque(false);
         this.setLayout(new BorderLayout());
+        //setting background color, layout of this class
 
         JPanel mainPanel = new JPanel();
         mainPanel.setMaximumSize(new Dimension(500,200));
@@ -22,10 +17,11 @@ public class EndScreen extends JPanel implements MouseInputListener{
         mainPanel.setOpaque(false);
         BoxLayout bl = new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS);
         mainPanel.setLayout(bl);
+        //creating mainPanel, setting its maximum size, layout, and transparency
+
         JPanel fillerx = new JPanel();
         fillerx.setPreferredSize(new Dimension(150,Chess.board.getHeight()));
         fillerx.setOpaque(false);
-
         JPanel fillerx2 = new JPanel();
         fillerx2.setOpaque(false);
         fillerx2.setPreferredSize(new Dimension(150,Chess.board.getHeight()));
@@ -35,7 +31,8 @@ public class EndScreen extends JPanel implements MouseInputListener{
         JPanel fillery2 = new JPanel();
         fillery2.setOpaque(false);
         fillery2.setPreferredSize(new Dimension(Chess.board.getWidth(),200));
-
+        //Creating filler panels to fill the empty spaces in the layout
+        
         JLabel label = new JLabel(status);
         label.setForeground(Color.white);
         label.setFont(new Font("Verdana", Font.BOLD,48));
@@ -51,21 +48,27 @@ public class EndScreen extends JPanel implements MouseInputListener{
         mainPanel.add(label);
         mainPanel.add(label2);
         mainPanel.add(Box.createVerticalGlue());
+        //creating 2 labels, setting its font, color, alignment, and adding it to mainPanel
         
         button.setSize(new Dimension(300,200));
         button.setForeground(Color.white);
         button.setBackground(Color.decode("#534138"));
         button.setOpaque(true);
+       
         button.setFont(new Font("Verdana", Font.BOLD,36));
         button.setAlignmentX(CENTER_ALIGNMENT);
         button.addMouseListener(this);
         mainPanel.add(button);
+        //setting size, font, color, alignment and adding mouselistener to "New Game" button, then adding it to mainPanel
+
+
 
         this.add(fillerx, BorderLayout.WEST);
         this.add(fillery, BorderLayout.NORTH);
         this.add(fillerx2, BorderLayout.EAST);
         this.add(fillery2, BorderLayout.SOUTH);
         this.add(mainPanel,BorderLayout.CENTER);
+        //adding all the panels to the EndScreen class's layout
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -73,6 +76,7 @@ public class EndScreen extends JPanel implements MouseInputListener{
         Rectangle r = g.getClipBounds();
         g.fillRect(r.x, r.y, r.width, r.height);
         super.paintComponent(g);
+        //overriding the paintComponent method to set the background color 
     }
 
     @Override
@@ -83,6 +87,7 @@ public class EndScreen extends JPanel implements MouseInputListener{
             Piece.moves = Piece.filterMoves();
             this.setVisible(false);
         }else{
+            //when button is clicked, it will load the starting position of the chess game, set the turn to white, filter the moves and make the end screen invisible.
             e.consume();
         }
         
